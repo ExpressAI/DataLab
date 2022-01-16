f53830515758fae4a2656d64833271f6ae4d08b6
Weizhe Yuan <50064641+yyy-Apple@users.noreply.github.com>
2022-01-16 21:05:34 +0800

    Add files via upload

--- src/datalabs/operations/prompt/natural_language_inference.py
+++ src/datalabs/operations/prompt/natural_language_inference.py
@@ -3,29 +3,30 @@ from typing import Callable, Mapping
 
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from operation import DatasetOperation, dataset_operation
 from prompt.prompting import *
 
+
 class NLIPrompting(Prompting, DatasetOperation):
 
-
     def __init__(self,
-                 name:str = None,
-                 func:Callable[...,Any] = None,
+                 name: str = None,
+                 func: Callable[..., Any] = None,
                  resources: Optional[Mapping[str, Any]] = None,
                  contributor: str = None,
                  processed_fields: List = ["text", "label"],
                  generated_field: str = None,
-                 task = "natural-language-inference",
-                 description = None,
-                 template = None,
+                 task="natural-language-inference",
+                 description=None,
+                 template=None,
                  ):
-        super().__init__(name = name, func = func, resources = resources, contributor = contributor,
-                         task = task,description=description)
+        super().__init__(name=name, func=func, resources=resources, contributor=contributor,
+                         task=task, description=description)
         self._type = 'NLIPrompting'
         self.processed_fields = ["text", "label"]
-        if isinstance(processed_fields,str):
+        if isinstance(processed_fields, str):
             self.processed_fields[0] = processed_fields
         else:
             self.processed_fields = processed_fields
@@ -33,7 +34,7 @@ class NLIPrompting(Prompting, DatasetOperation):
         self._data_type = "Dataset"
         self.template = template
 
-    def __call__(self, sample, labels_to_answers) -> Any: # str?
+    def __call__(self, sample, labels_to_answers) -> Any:  # str?
         """
         Parameters
         x: Text
@@ -50,35 +51,33 @@ class nli_prompting(prompting, dataset_operation):
                  resources: Optional[Mapping[str, Any]] = None,
                  contributor: str = None,
                  processed_fields: List = ["text", "label"],
-                 generated_field:str = None,
-                 task = "natural-language-inference",
-                 description = None,
-                 template = None,
+                 generated_field: str = None,
+                 task="natural-language-inference",
+                 description=None,
+                 template=None,
                  ):
-        super().__init__(name = name, resources = resources, contributor = contributor, description=description)
+        super().__init__(name=name, resources=resources, contributor=contributor, description=description)
         self.processed_fields = processed_fields
         self.generated_field = generated_field
         self.task = task
         self.template = template
 
-
     def __call__(self, *param_arg):
         if callable(self.name):
-            tf_class = NLIPrompting(name = self.name.__name__, func=self.name)
+            tf_class = NLIPrompting(name=self.name.__name__, func=self.name)
             return tf_class(*param_arg)
         else:
             f = param_arg[0]
             name = self.name or f.__name__
-            tf_cls = NLIPrompting(name=name, func = f,
-                                   resources = self.resources,
-                                   contributor = self.contributor,
-                                    processed_fields = self.processed_fields,
-                                    generated_field = self.generated_field,
-                                    task = self.task,
-                                    description=self.description,
-                                    template = self.template,)
+            tf_cls = NLIPrompting(name=name, func=f,
+                                  resources=self.resources,
+                                  contributor=self.contributor,
+                                  processed_fields=self.processed_fields,
+                                  generated_field=self.generated_field,
+                                  task=self.task,
+                                  description=self.description,
+                                  template=self.template, )
             return tf_cls
-
 
 
 """
@@ -101,24 +100,22 @@ print(next(res))
 
 """
 
-@nli_prompting(name = "template_p1", contributor= "datalab", processed_fields= ['text1', 'text2', 'label'],
-                               template = "TEXT: {text1} PROMPT: Can we infer that {text2}? Yes or No or Unknown?",
-                                 task="natural-language-inference", description="this function is used to calculate the text length")
-def template_p1(sample:dict, labels_to_answers:Dict):
 
+@nli_prompting(name="template_p1", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="Given that {text1} Can we infer that {text2}? Yes or No or Unknown?",
+               task="natural-language-inference")
+def template_p1(sample: dict, labels_to_answers: Dict):
     # labels=('contradiction', 'entailment', 'neutral'))
-    answers_to_desc = {'contradiction':"no",
-                       'entailment':"yes",
-                       'neutral':'unknown'}
+    answers_to_desc = {'contradiction': "No",
+                       'entailment': "Yes",
+                       'neutral': 'Unknown'}
 
-    tp1 = "TEXT: {text1} PROMPT: Can we infer that {text2}? Yes or No or Unknown?"
+    tp1 = "Given that {text1} Can we infer that {text2}? Yes or No or Unknown?"
 
     # prompting process
     answers = list(labels_to_answers.values())
     text1 = sample["text1"]
     text2 = sample["text2"]
-
-
 
     # instantiation
     text_prompt = eval("f'{}'".format(tp1))
@@ -129,13 +126,10 @@ def template_p1(sample:dict, labels_to_answers:Dict):
             "label_prompt": label_prompt}
 
 
-
-
-@nli_prompting(name = "template_p1", contributor= "datalab", processed_fields= ['text1', 'text2', 'label'],
-                               template = "Given text {text1} and text {text2}, is their relationship {texture_choices}",
-                                 task="natural-language-inference", description="this function is used to calculate the text length")
-def template_p2(sample:dict, labels_to_answers:Dict):
-
+@nli_prompting(name="template_p1", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="Given text {text1} and text {text2}, is their relationship {texture_choices}",
+               task="natural-language-inference")
+def template_p2(sample: dict, labels_to_answers: Dict):
     tp = "Given text: {text1} and text: {text2}, is their relationship {texture_choices}"
 
     # prompting process
@@ -143,7 +137,6 @@ def template_p2(sample:dict, labels_to_answers:Dict):
     text2 = sample["text2"]
     answers = list(labels_to_answers.values())
     texture_choices = ", ".join(answers[:-1]) + " or " + answers[-1] + "?"
-
 
     # instantiation
     text_prompt = eval("f'{}'".format(tp))
@@ -154,23 +147,18 @@ def template_p2(sample:dict, labels_to_answers:Dict):
             "label_prompt": label_prompt}
 
 
-
-@nli_prompting(name = "template_p3", contributor= "datalab", processed_fields= ['text1', 'text2', 'label'],
-                               template = "The relationship of two texts {text1} and {text2} is [mask].",
-                                 task="natural-language-inference", description="this function is used to calculate the text length")
-def template_p3(sample:dict, labels_to_answers:Dict):
-
+@nli_prompting(name="template_p3", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="The relationship of two texts {text1} and {text2} is [mask]",
+               task="natural-language-inference")
+def template_p3(sample: dict, labels_to_answers: Dict):
     # labels=('contradiction', 'entailment', 'neutral'))
 
-
-    tp1 = "The relationship of two texts {text1} and {text2} is [mask]."
+    tp1 = "The relationship of two texts {text1} and {text2} is [mask]"
 
     # prompting process
 
     text1 = sample["text1"]
     text2 = sample["text2"]
-
-
 
     # instantiation
     text_prompt = eval("f'{}'".format(tp1))
@@ -181,21 +169,147 @@ def template_p3(sample:dict, labels_to_answers:Dict):
             "label_prompt": label_prompt}
 
 
-# @text_classification_prompting(name = "template_p1", contributor= "datalab", processed_fields= ['text', 'label'],
-#                                  task="text-classification", description="this function is used to calculate the text length")
-# def template_p1(sample:dict, labels_to_answers:Dict, template:str):
-#
-#     template = "Given the text: {text}, is it about {all_answer_options}"
-#
-#     # prompting process
-#     answers = list(labels_to_answers.values())
-#     text = sample["text"]
-#     all_answer_options = ", ".join(answers[:-1]) + " or " + answers[-1] + "?"
-#
-#     # instantiation
-#     text_prompt = eval("f'{}'".format(template))
-#
-#     label_prompt = labels_to_answers[sample["label"]]
-#
-#     return {"text_prompt": text_prompt,
-#             "label_prompt": label_prompt}
+@nli_prompting(name="template_p4", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="{text1} {text2} True or False or Unknown?",
+               task="natural-language-inference")
+def template_p4(sample: dict, labels_to_answers: Dict):
+    answers_to_desc = {'contradiction': "False",
+                       'entailment': "True",
+                       'neutral': 'Unknown'}
+    tp = "{text1} {text2} True or False or Unknown?"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = answers_to_desc[labels_to_answers[sample["label"]]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p5", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="{text1} Is the following statement True or False or Unknown: {text2}?",
+               task="natural-language-inference")
+def template_p5(sample: dict, labels_to_answers: Dict):
+    answers_to_desc = {'contradiction': "False",
+                       'entailment': "True",
+                       'neutral': 'Unknown'}
+    tp = "{text1} Is the following statement True or False or Unknown: {text2}?"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = answers_to_desc[labels_to_answers[sample["label"]]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p6", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="Premise: {text1} Hypothesis: {text2} Based on the premise, is the hypothesis true or false or undetermined?",
+               task="natural-language-inference")
+def template_p6(sample: dict, labels_to_answers: Dict):
+    answers_to_desc = {'contradiction': "false",
+                       'entailment': "true",
+                       'neutral': 'undetermined'}
+    tp = "Premise: {text1} Hypothesis: {text2} Based on the premise, is the hypothesis true or false or undetermined?"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = answers_to_desc[labels_to_answers[sample["label"]]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p7", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="Premise: {text1} Hypothesis: {text2} The relation between the hypothesis and premise is [mask]",
+               task="natural-language-inference")
+def template_p7(sample: dict, labels_to_answers: Dict):
+    tp = "Premise: {text1} Hypothesis: {text2} The relation between the hypothesis and premise is [mask]"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = labels_to_answers[sample["label"]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p7", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="Premise: {text1} Hypothesis: {text2} The relation between the hypothesis and premise is [mask]",
+               task="natural-language-inference")
+def template_p7(sample: dict, labels_to_answers: Dict):
+    tp = "Premise: {text1} Hypothesis: {text2} The relation between the hypothesis and premise is [mask]"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = labels_to_answers[sample["label"]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p8", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="{text1} Based on that information, is the claim \"{text2}\" true, false or inconclusive?",
+               task="natural-language-inference")
+def template_p8(sample: dict, labels_to_answers: Dict):
+    answers_to_desc = {'contradiction': "false",
+                       'entailment': "true",
+                       'neutral': 'inconclusive'}
+    tp = "{text1} Based on that information, is the claim \"{text2}\" true, false or inconclusive?"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = answers_to_desc[labels_to_answers[sample["label"]]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p9", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="{text1} Does it imply that \"{text2}\"? Yes, No or Maybe?",
+               task="natural-language-inference")
+def template_p9(sample: dict, labels_to_answers: Dict):
+    answers_to_desc = {'contradiction': "No",
+                       'entailment': "Yes",
+                       'neutral': 'Maybe'}
+    tp = "{text1} Does it imply that \"{text2}\"? Yes, No or Maybe?"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = answers_to_desc[labels_to_answers[sample["label"]]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
+
+
+@nli_prompting(name="template_p10", contributor="datalab", processed_fields=['text1', 'text2', 'label'],
+               template="Assume it is true that {text1}. Therefore, {text2} is guaranteed, possible or impossible?",
+               task="natural-language-inference")
+def template_p10(sample: dict, labels_to_answers: Dict):
+    answers_to_desc = {'contradiction': "impossible",
+                       'entailment': "guaranteed",
+                       'neutral': 'possible'}
+    tp = "Assume it is true that {text1}. Therefore, {text2} is guaranteed, possible or impossible?"
+    text1 = sample["text1"]
+    text2 = sample["text2"]
+
+    # instantiation
+    text_prompt = eval("f'{}'".format(tp))
+    label_prompt = answers_to_desc[labels_to_answers[sample["label"]]]
+
+    return {"text_prompt": text_prompt,
+            "label_prompt": label_prompt}
