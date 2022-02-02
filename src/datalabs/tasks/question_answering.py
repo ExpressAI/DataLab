e50df7ef7c5daa3cfc27f6407c66262a56a56b43
Jinlan Fu <16110240002@fudan.edu.cn>
2022-02-02 18:15:23 +0800

    add hotpot
    add hotpot

--- src/datalabs/tasks/question_answering.py
+++ src/datalabs/tasks/question_answering.py
@@ -1,16 +1,3 @@
-# coding=utf-8
-# Copyright 2022 The HuggingFace Datasets, DataLab Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from dataclasses import dataclass
 from typing import ClassVar, Dict
 
@@ -41,3 +28,54 @@ class QuestionAnsweringExtractive(TaskTemplate):
     @property
     def column_mapping(self) -> Dict[str, str]:
         return {self.question_column: "question", self.context_column: "context", self.answers_column: "answers"}
+
+
+
+@dataclass
+class QuestionAnsweringHotpot(TaskTemplate):
+    # `task` is not a ClassVar since we want it to be part of the `asdict` output for JSON serialization
+    task_category: str = "question-answering-hotpot"
+    task: str = "question-answering-hotpot"
+
+    # "supporting_facts": {
+    #     "title": supporting_titles,
+    #     "sent_id": supporting_sent_ids,
+    # },
+    # "context": {
+    #     "title": context_titles,
+    #     "sentences": context_sentences,
+    # },
+
+    input_schema: ClassVar[Features] = Features({"question": Value("string"),
+                                                 "context": Sequence(
+                                                        {
+                                                            "text": Value("string"),
+                                                            "sentences": Value("string"),
+                                                        }
+                                                 )
+                                                 })
+    label_schema: ClassVar[Features] = Features(
+        {
+            "answers": Sequence(
+                {
+                    "text": Value("string"),
+                    "answer_start": Value("int32"),
+                }
+            ),
+            "supporting_facts": Sequence(
+                {
+                    "title": Value("string"),
+                    "sent_id": Value("int32"),
+                }
+            )
+        }
+    )
+    question_column: str = "question"
+    context_column: str = "context"
+    answers_column: str = "answers"
+    supporting_column: str = "supporting_facts"
+
+
+    @property
+    def column_mapping(self) -> Dict[str, str]:
+        return {self.question_column: "question", self.context_column: "context", self.answers_column: "answers", self.supporting_column:"supporting_facts"}
