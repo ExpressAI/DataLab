a79a0720f1ac7a3a206ecb4d776dc23b093604be
Hzfinfdu <19307110020@fudan.edu.cn>
2022-02-07 08:28:28 -0500

    add relation extraction

--- src/datalabs/tasks/__init__.py
+++ src/datalabs/tasks/__init__.py
@@ -10,6 +10,7 @@ from .text_classification import TextClassification
 from .text_matching import TextMatching
 from .sequence_labeling import SequenceLabeling
 from .semantic_parsing import SemanticParsing
+from .relation_extraction import RelationExtraction
 
 
 __all__ = [
@@ -24,6 +25,7 @@ __all__ = [
     "TextMatching",
     "SequenceLabeling",
     "SemanticParsing",
+    "RelationExtraction",
 ]
 
 logger = get_logger(__name__)
@@ -40,6 +42,7 @@ NAME2TEMPLATE = {
     TextMatching.task_category:TextMatching,
     SequenceLabeling.task_category: SequenceLabeling,
     SemanticParsing.task_category: SemanticParsing,
+    RelationExtraction.task_category: RelationExtraction,
 }
 
 
