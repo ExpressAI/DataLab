4ec57bf28e6c74d6a293a0b524edc7bdb8c68d93
Jinlan Fu <16110240002@fudan.edu.cn>
2022-02-08 22:11:16 +0800

    update copyright
    update copyright

--- datasets/drop/drop.py
+++ datasets/drop/drop.py
@@ -1,5 +1,15 @@
-"""Dataset config script for ag_news （this code is originally from huggingface, them modified by datalab）"""
-
+# coding=utf-8
+# Copyright 2020 The HuggingFace Datasets Authors and the TensorFlow Datasets Authors and DataLab Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 import json
@@ -75,6 +85,7 @@ class Drop(datalabs.GeneratorBasedBuilder):
             # datasets.features.FeatureConnectors
             features=datalabs.Features(
                 {
+                    "id": datalabs.Value("string"),
                     "section_id": datalabs.Value("string"),
                     "query_id": datalabs.Value("string"),
                     "context": datalabs.Value("string"), # context->passage
@@ -136,6 +147,7 @@ class Drop(datalabs.GeneratorBasedBuilder):
                 for j, qa in enumerate(section["qa_pairs"]):
 
                     example = {
+                        "id":str(id_),
                         "section_id": section_id,
                         "query_id": qa["query_id"],
                         "context": section["passage"],
