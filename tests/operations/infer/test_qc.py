859416a73c02693384c9244023b7332136b64bfe
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-03-06 10:03:18 -0500

    Create test_qc.py

---
+++ tests/operations/infer/test_qc.py
@@ -0,0 +1,33 @@
+import unittest
+from datalabs import load_dataset
+from datalabs.operations.infer.inference import inference
+from datalabs.operations.aggregate.auto_eval import explainaboard
+
+
+@inference(name="my_inference")
+def my_inference(samples: list):
+    predictions = []
+    for sample in samples:
+        text, label = sample["text"], sample["label"]
+
+        prediction = label # this should be modified based on user's model
+
+        predictions.append({"prediction": prediction})
+
+    return predictions
+
+
+class MyTestCase(unittest.TestCase):
+    def test_general(self):
+        # load dataset
+        dataset = load_dataset("qc")
+        # inference over test set based on a machine learning model
+        test_data = dataset["test"].apply(my_inference, mode="memory")
+        # evaluation
+        test_data = test_data.apply(explainaboard)
+        # print the result
+        print(test_data._stat)
+
+
+if __name__ == '__main__':
+    unittest.main()
