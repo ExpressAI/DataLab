36eed9d3e409f84586d7b4df8c8235c3b07910eb
yyy-Apple <50064641+yyy-Apple@users.noreply.github.com>
2022-03-06 16:21:59 +0800

    update inference

--- tests/operations/infer/test_general.py
+++ tests/operations/infer/test_general.py
@@ -1,48 +1,40 @@
 import unittest
-
 from datalabs import load_dataset
 from datalabs.operations.infer.inference import inference
 from datalabs.operations.aggregate.auto_eval import explainaboard
 
 
 @inference(name="my_inference")
-def my_inference(samples: dict):
+def my_inference(samples: list):
+    """
+    This is an example using Huggingface Pipeline to do sentiment classification
+    One thing we need to notice is that in Datalab, positive is 0, negative is 1
+    """
+    from transformers import pipeline
+    classifier = pipeline('sentiment-analysis')
+    examples = [x["text"] for x in samples]
+    outs = classifier(examples)
     predictions = []
-    for sample in samples:
-        text, label = sample["text"], sample["label"]
-
-        prediction = label # this should be modified based on user's model
-
-        predictions.append({"prediction": prediction})
-
+    for out in outs:
+        if out["label"] == "POSITIVE":
+            # In DataLab, positive is 0, negative is 1
+            predictions.append({"prediction": 0})
+        else:
+            predictions.append({"prediction": 1})
     return predictions
 
+
 class MyTestCase(unittest.TestCase):
-
-
-
-
-
     def test_general(self):
-
         # load dataset
-        dataset = load_dataset("qc")
+        dataset = load_dataset("sst2")
         # inference over test set based on a machine learning model
-        test_data = dataset["test"].apply(my_inference, mode = "memory")
+        test_data = dataset["test"].apply(my_inference, mode="memory")
         # evaluation
         test_data = test_data.apply(explainaboard)
-
         # print the result
         print(test_data._stat)
 
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
