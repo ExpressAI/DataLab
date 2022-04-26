297fdc32ca30ef86b6cb164f4d55693f07a98d88
Pengfei Liu <stefanpengfei@gmail.com>
2022-04-25 22:36:25 -0400

    Create openapi.py

---
+++ client/openapi.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+# Search by language
+path = 'https://datalab.nlpedia.ai/api/fetch_data_for_sdk_language'
+data = {
+    'language': 'fr'
+}
+r = requests.post(path, json=data)
+list_ = json.loads(r.content)
+print(len(list_))
+print(list_[0])
+
+
+# Search by task
+path = 'https://datalab.nlpedia.ai/api/fetch_data_for_sdk_task'
+data = {
+    'task': 'sentiment-classification',
+}
+r = requests.post(path, json=data)
+list_ = json.loads(r.content)
+print(len(list_))
+print(list_[0])
+
+
+
+# Get all languages, tasks and task_category
+path = 'https://datalab.nlpedia.ai/api/fetch_meta_data_for_sdk_task_language'
+r = requests.get(path)
+dic_ = json.loads(r.content)
+print(dic_['languages'])  # list of all languages
+print(dic_['tasks'])  # list of all tasks
+print(dic_['task_categories'])  # list of all task_category
