fa0804d965de3130b2e05d16f2dda90fa752f688
ffaisal93 <fahimfaisal@iut-dhaka.edu>
2022-06-14 19:59:21 -0400

    sdqa language specific files added

--- datasets/sdqa/sdqa.py
+++ datasets/sdqa/sdqa.py
@@ -45,35 +45,21 @@ is a multi-dialect, spoken QA benchmark on five languages (Arabic, Bengali, Engl
 with more than 68k audio prompts in 22 dialects from 245 speakers. Google Speech API with regional units 
 (eg. en-US, sw-TZ) are used to perform speech to text conversion of the questions. These questions are
 replaced in place of the original TyDiQA gold questions to prepare dialectal development and test set. As a benchmark 
-train set, a discarded version of original TyDiQA training set is used.
+train set, a discarded subset of original TyDiQA training set is used.
 
 """
 
-# LANG_URLS = {
-#     "ar": "https://drive.google.com/uc?export=download&id=1tUsnP9xPFEsLjShtnsN1osgK84jPc3ED",
-#     "bn": "https://drive.google.com/uc?export=download&id=1qqOiZy1lz0pIzw_rgrVHwx2XJveIJzGb",
-#     "en": "https://drive.google.com/uc?export=download&id=1WGfgc7_0DCkxBsheaxfke4TK_j1G5OFT",
-#     "fi": "https://drive.google.com/uc?export=download&id=1-pbAbt-v2QHWDj2dnbKcOSNbJHLEiGpd",
-#     "id": "https://drive.google.com/uc?export=download&id=1QuMrKvWJ74sl-MrTAo6Mz-_bs953YZM6",
-#     "ko": "https://drive.google.com/uc?export=download&id=1PzbuXVeCuYU3CBPLgqjM4GuRvY1fCvVl",
-#     "ru": "https://drive.google.com/uc?export=download&id=1NIlGs1o_1AYPlyIIcBnXJHIRh5Zi9nU1",
-#     "sw": "https://drive.google.com/uc?export=download&id=1JkXf6rHyFRWyQGTCdwIxPVuRoY2dmDwd",
-#     "te": "https://drive.google.com/uc?export=download&id=1NyliC5GairK8TvxrQOvH-ktNgsKbA5Fz",
-# }
-# DATA_URL="https://drive.google.com/uc?export=download&id=1_kGzqqiJJLGpKN6pfNzguyzG_vU8figf"
-# TRAIN_LANGS = {'en':'english','bn':'bengali','sw':'swahili','ko':'korean','ar':'arabic','fi':'finnish','te':'telugu','id':'indonesian','ru':'russian'}
-# TRAIN_LANGS = {'en':'english','bn':'bengali'}
-# SDQA_LANGS = {'en':'english','bn':'bengali','sw':'swahili','ko':'korean','ar':'arabic'}
-# DIALECTS = {
-#     'english':['nga','nzl','usa','ind_s','ind_n','irl','kenya','gbr','zaf','aus','phl'],
-#     'arabic':['bhr','dza','egy','jor','mar','sau','tun'],
-#     'swahili':['kenya','tanzania'],
-#     'bengali':['dhaka','ind'],
-#     'korean':['korn','kors']
-# }
+LANG_URLS = {
+    "ara": "https://drive.google.com/uc?export=download&id=1AUpczDwqwjnw44Trfu7ku-DQ533cmfVG",
+    "ben": "https://drive.google.com/uc?export=download&id=1iP0r3JbF4P3QDkrHCHdIXsVFv-pUac3N",
+    "eng": "https://drive.google.com/uc?export=download&id=1-XdM9TcDFACj7OXLi0KlTXnpwE2EgjN9",
+    "kor": "https://drive.google.com/uc?export=download&id=120YpOhUje06yuTM58kXdgs1aBmrmmHDG",
+    "swa": "https://drive.google.com/uc?export=download&id=1KjCthk7MDVz2IlznIsgmcOzQ39Iqe33u",
+}
 
 
-_URLS = {
+
+ALL_URLS = {
     "train": "https://drive.google.com/uc?export=download&id=16M6bglY6ttiMcYximOkQ5jKX12KyRYxL",
     "dev": "https://drive.google.com/uc?export=download&id=18SNCH2uQ7PBKAeOWgyRlWS_QmbJi3oDj",
     "test": "https://drive.google.com/uc?export=download&id=18iyLut6C6SVu3cZFlocBGtarFgQ03qzn",
@@ -96,7 +82,12 @@ class Sdqa(datalabs.GeneratorBasedBuilder):
 
     # TODO(squad_v2): Set up version.
     BUILDER_CONFIGS = [
-        SdqaConfig(name="sdqa", version=datalabs.Version("1.0.0"), description="SDQA 1.0.0"),
+        datalabs.BuilderConfig(
+            name="{}".format(lang),
+            version=datalabs.Version("2.0.0"),
+            description="SDQA 1.0.0"
+        )
+        for lang in list(LANG_URLS.keys())
     ]
 
     def _info(self):
@@ -126,7 +117,7 @@ class Sdqa(datalabs.GeneratorBasedBuilder):
             # Homepage of the dataset for documentation
             homepage="https://nlp.cs.gmu.edu/publication/faisal-etal-21-sdqa/",
             citation=_CITATION,
-            languages=["en","ar","bn","sw","ko"],
+            languages=["eng","ara","ben","swa","kor"],
             task_templates=[
                 get_task(TaskType.qa_extractive)(
                     question_column="question", context_column="context", answers_column="answers"
@@ -139,13 +130,39 @@ class Sdqa(datalabs.GeneratorBasedBuilder):
         # TODO(squad_v2): Downloads the data and defines the splits
         # dl_manager is a datalab.download.DownloadManager that can be used to
         # download and extract URLs
-        urls_to_download = _URLS
-        downloaded_files = dl_manager.download_and_extract(urls_to_download)
+        # urls_to_download = _URLS
+        # downloaded_files = dl_manager.download_and_extract(urls_to_download)
 
+        # return [
+        #     datalabs.SplitGenerator(name=datalabs.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
+        #     datalabs.SplitGenerator(name=datalabs.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
+        #     datalabs.SplitGenerator(name=datalabs.Split.TEST, gen_kwargs={"filepath": downloaded_files["test"]}),
+        # ]
+
+        lang = str(self.config.name)
+        url =LANG_URLS[lang]
+        # url = _URL.format(lang, self.VERSION.version_str[:-2])
+        data_dir = dl_manager.download_and_extract(url)
         return [
-            datalabs.SplitGenerator(name=datalabs.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
-            datalabs.SplitGenerator(name=datalabs.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
-            datalabs.SplitGenerator(name=datalabs.Split.TEST, gen_kwargs={"filepath": downloaded_files["test"]}),
+            datalabs.SplitGenerator(
+                name=datalabs.Split.TRAIN,
+                gen_kwargs={
+                    "filepath": os.path.join(data_dir,lang,"sdqa-train-" + lang + ".json"),
+                    # "filepath": os.path.join(data_dir,  "train-en.json" + lang + "_train.jsonl"),
+                },
+            ),
+            datalabs.SplitGenerator(
+                name=datalabs.Split.TEST,
+                gen_kwargs={
+                    "filepath": os.path.join(data_dir,lang, "sdqa-test-" + lang + ".json"),
+                },
+            ),
+            datalabs.SplitGenerator(
+                name=datalabs.Split.VALIDATION,
+                gen_kwargs={
+                    "filepath": os.path.join(data_dir,lang, "sdqa-dev-" + lang + ".json"),
+                },
+            ),
         ]
 
     def _generate_examples(self, filepath):
