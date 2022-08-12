d4099525dab5931be5ced62152330a77bd0732b4
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-08-11 23:37:00 -0400

    Update ranking.py

--- datalabs/tasks/ranking.py
+++ datalabs/tasks/ranking.py
@@ -21,8 +21,8 @@ class Ranking(TaskTemplate):
         if self.input_schema is None:
             self.input_schema: ClassVar[Features] = Features(
                 {
-                    self.context_column: Sequence(Value("string")),
-                    self.utterance_column: Value("string"),
+                    self.context_column: Value("string"),
+                    self.utterance_column: Sequence(Value("string")),
                 }
             )
         if self.label_schema is None:
@@ -33,6 +33,16 @@ class Ranking(TaskTemplate):
             )
 
 
+@register_task(TaskType.argument_pair_identification)
+@dataclass
+class ArgumentPairIdentification(Ranking):
+    task: TaskType = TaskType.argument_pair_identification
+    context_column: str = "quotation"  
+    utterance_column: str = "replies"  # a list of replies, including positive and negative reply
+    label_column: str = "label"                 
+                
+                
+                
 @register_task(TaskType.retrieval_based_dialogue)
 @dataclass
 class RetrievalBasedDialogue(Ranking):
@@ -41,3 +51,23 @@ class RetrievalBasedDialogue(Ranking):
     context_column: str = "context"
     utterance_column: str = "utterance"
     label_column: str = "label"
+        
+    def __post_init__(self):
+        self.task_categories = [
+            task_cls.get_task() for task_cls in self.get_task_parents()
+        ]
+
+        if self.input_schema is None:
+            self.input_schema: ClassVar[Features] = Features(
+                {
+                    self.context_column: Sequence("string"),
+                    self.utterance_column: Sequence(Value("string")),
+                }
+            )
+        if self.label_schema is None:
+            self.label_schema: ClassVar[Features] = Features(
+                {
+                    self.label_column: Value("int32"),
+                }
+            )        
+        
