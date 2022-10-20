73f66a040e34ba03e3a2d7f93f720461b209e1af
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-10-19 23:01:49 -0400

    add wmt20 (#369)

---
+++ datasets/wmt20/wmt20.py
@@ -0,0 +1,117 @@
+import json
+import csv
+import datalabs
+from datalabs import get_task, TaskType
+from datalabs.utils.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+_CITATION = """
+"""
+
+
+_DESCRIPTION = """\
+WMT20 
+"""
+
+_URL = "https://datalab-hub.s3.amazonaws.com/wmt20_test/{}/test.tsv"
+_LANGUAGES = [
+    "csen",
+    "defr", #
+    "encs",
+    "enja",
+    "enpl",
+    "enru", #
+    "enta",
+    "enzh", #
+    "frde", #
+    "iuen",
+    "kmen",
+    "plen", #
+    "psen",
+    "ruen", #
+    "taen",
+]
+
+
+
+
+class WMT20Config(datalabs.BuilderConfig):
+    """BuilderConfig for MConala."""
+
+    def __init__(self, **kwargs):
+        """BuilderConfig for WMT20.
+        Args:
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(WMT20Config, self).__init__(**kwargs)
+
+
+class WMT20(datalabs.GeneratorBasedBuilder):
+
+    BUILDER_CONFIGS = [
+        datalabs.BuilderConfig(
+            name=lang,
+        )
+        for lang in list(_LANGUAGES)
+    ]
+
+    def _info(self):
+        features_sample = datalabs.Features(
+            {
+                "translation": {
+                    self.config.name[0:2]: datalabs.Value("string"),
+                    self.config.name[2:4]: datalabs.Value("string"),
+                },
+            }
+        )
+
+        return datalabs.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            citation=_CITATION,
+            # Homepage of the dataset for documentation
+            homepage="https://www.statmt.org/wmt20/",
+            # datasets.features.FeatureConnectors
+            features=features_sample,
+            supervised_keys=None,
+            languages=[self.config.name[0:2],
+                       self.config.name[2:4]],
+            task_templates=[
+                get_task(TaskType.machine_translation)(
+                    translation_column="translation",
+                    lang_sub_columns=[self.config.name[0:2],
+                                      self.config.name[2:4]],
+                )
+            ],
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+
+        lang = self.config.name
+        dataset_path = dl_manager.download_and_extract(_URL.format(lang))
+
+        return [
+            datalabs.SplitGenerator(
+                name="test",
+                gen_kwargs={"filepath": f"{dataset_path}"},
+            ),
+        ]
+
+
+
+    def _generate_examples(self, filepath):
+        """Yields examples."""
+        with open(filepath, encoding="utf-8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter="\t")
+            for id_, row in enumerate(csv_reader):
+                if len(row) == 1:
+                    src, ref = row[0], ""
+                else:
+                    src, ref = row[0], row[1]
+
+
+                yield id_, {"translation": {self.config.name[0:2]: src,
+                                        self.config.name[2:4]: ref}}
