88f5f62e8c75b9e292dc961b8154270811097213
Graham Neubig <neubig@gmail.com>
2022-10-21 22:50:40 -0400

    Add CMU ANLP sciner (#370)

--- datasets/cmu_anlp/cmu_anlp.py
+++ datasets/cmu_anlp/cmu_anlp.py
@@ -34,6 +34,7 @@ class CmuAnlp(datalabs.GeneratorBasedBuilder):
     BUILDER_CONFIGS = [
         datalabs.BuilderConfig(name='sst'),
         datalabs.BuilderConfig(name='cfimdb'),
+        datalabs.BuilderConfig(name='sciner'),
     ]
 
     @staticmethod
@@ -51,6 +52,24 @@ class CmuAnlp(datalabs.GeneratorBasedBuilder):
                 "0": "negative",
                 "1": "positive",
             }
+        elif subdataset.startswith('sciner'):
+            return {
+                "0": "O",
+                "1": "B-DatasetName",
+                "2": "B-HyperparameterName",
+                "3": "B-HyperparameterValue",
+                "4": "B-MethodName",
+                "5": "B-MetricName",
+                "6": "B-MetricValue",
+                "7": "B-TaskName",
+                "8": "I-DatasetName",
+                "9": "I-HyperparameterName",
+                "10": "I-HyperparameterValue",
+                "11": "I-MethodName",
+                "12": "I-MetricName",
+                "13": "I-MetricValue",
+                "14": "I-TaskName",
+            }
         else:
             raise ValueError(f'bad subdataset {subdataset}')
 
@@ -58,51 +77,114 @@ class CmuAnlp(datalabs.GeneratorBasedBuilder):
 
         labels = list(self._get_labels_for_subdataset(self.config.name).values())
 
-        return datalabs.DatasetInfo(
-            description=_DESCRIPTION,
-            features=datalabs.Features(
-                {
-                    "text": datalabs.Value("string"),
-                    "label": datalabs.features.ClassLabel(
-                        names=labels
-                    ),
-                }
-            ),
-            homepage="https://phontron.com/class/anlp2022/",
-            citation=_CITATION,
-            languages=["en"],
-            task_templates=[
-                get_task(TaskType.sentiment_classification)(
-                    text_column="text", label_column="label"
-                )
-            ],
-        )
+        if self.config.name == 'sciner':
+            return datalabs.DatasetInfo(
+                description=_DESCRIPTION,
+                features=datalabs.Features(
+                    {
+                        "id": datalabs.Value("string"),
+                        "tokens": datalabs.Sequence(datalabs.Value("string")),
+                        "tags": datalabs.Sequence(
+                            datalabs.features.ClassLabel(names=labels)
+                        ),
+                    }
+                ),
+                supervised_keys=None,
+                homepage="https://phontron.com/class/anlp2022/",
+                citation=_CITATION,
+                task_templates=[
+                    get_task(TaskType.named_entity_recognition)(
+                        tokens_column="tokens", tags_column="tags"
+                    )
+                ],
+                languages=["en"],
+            )
+        else:
+            return datalabs.DatasetInfo(
+                description=_DESCRIPTION,
+                features=datalabs.Features(
+                    {
+                        "text": datalabs.Value("string"),
+                        "label": datalabs.features.ClassLabel(
+                            names=labels
+                        ),
+                    }
+                ),
+                homepage="https://phontron.com/class/anlp2022/",
+                citation=_CITATION,
+                languages=["en"],
+                task_templates=[
+                    get_task(TaskType.sentiment_classification)(
+                        text_column="text", label_column="label"
+                    )
+                ],
+            )
 
     def _split_generators(self, dl_manager):
 
-        train_path = dl_manager.download_and_extract(f'{_MINBERT_PREFIX}/{self.config.name}-train.txt')
-        validation_path = dl_manager.download_and_extract(f'{_MINBERT_PREFIX}/{self.config.name}-dev.txt')
-        test_path = dl_manager.download_and_extract(f'{_PRIVATE_PREFIX}/textclass/{self.config.name}-test.txt')
-        return [
-            datalabs.SplitGenerator(
-                name=datalabs.Split.TRAIN, gen_kwargs={"filepath": train_path}
-            ),
-            datalabs.SplitGenerator(
-                name=datalabs.Split.VALIDATION, gen_kwargs={"filepath": validation_path}
-            ),
-            datalabs.SplitGenerator(
-                name=datalabs.Split.TEST, gen_kwargs={"filepath": test_path}
-            ),
-        ]
+        if self.config.name == 'sciner':
+            test_path = dl_manager.download_and_extract(f'{_PRIVATE_PREFIX}/ner/{self.config.name}-test.conll')
+            return [
+                datalabs.SplitGenerator(
+                    name=datalabs.Split.TEST, gen_kwargs={"filepath": test_path}
+                ),
+            ]
+        else:
+            train_path = dl_manager.download_and_extract(f'{_MINBERT_PREFIX}/{self.config.name}-train.txt')
+            validation_path = dl_manager.download_and_extract(f'{_MINBERT_PREFIX}/{self.config.name}-dev.txt')
+            test_path = dl_manager.download_and_extract(f'{_PRIVATE_PREFIX}/textclass/{self.config.name}-test.txt')
+            return [
+                datalabs.SplitGenerator(
+                    name=datalabs.Split.TRAIN, gen_kwargs={"filepath": train_path}
+                ),
+                datalabs.SplitGenerator(
+                    name=datalabs.Split.VALIDATION, gen_kwargs={"filepath": validation_path}
+                ),
+                datalabs.SplitGenerator(
+                    name=datalabs.Split.TEST, gen_kwargs={"filepath": test_path}
+                ),
+            ]
 
     def _generate_examples(self, filepath):
-        """Generate SST2 examples."""
+        """Generate examples."""
 
-        # map the label into textual string
-        textualize_label = self._get_labels_for_subdataset(self.config.name)
+        if self.config.name == 'sciner':
 
-        with open(filepath, encoding="utf-8") as data_file:
-            for id_, line in enumerate(data_file):
-                label, text = line.strip().split(' ||| ')
-                label = textualize_label[label]
-                yield id_, {"text": text, "label": label}
+            with open(filepath, encoding="utf-8") as f:
+                guid = 0
+                tokens = []
+                tags = []
+                for line in f:
+                    if line.startswith("-DOCSTART-") or line == "" or line == "\n":
+                        if tokens:
+                            yield guid, {
+                                "id": str(guid),
+                                "tokens": tokens,
+                                "tags": tags,
+                            }
+                            guid += 1
+                            tokens = []
+                            tags = []
+                    else:
+                        # conll2003 tokens are space separated
+                        splits = line.split(" ")
+                        tokens.append(splits[0])
+                        tags.append(splits[3].rstrip())
+
+                # last example
+                if len(tokens) != 0:
+                    yield guid, {
+                        "id": str(guid),
+                        "tokens": tokens,
+                        "tags": tags,
+                    }
+
+        else:
+            # map the label into textual string
+            textualize_label = self._get_labels_for_subdataset(self.config.name)
+
+            with open(filepath, encoding="utf-8") as data_file:
+                for id_, line in enumerate(data_file):
+                    label, text = line.strip().split(' ||| ')
+                    label = textualize_label[label]
+                    yield id_, {"text": text, "label": label}
